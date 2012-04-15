17738e002f233759f7c08afb8a5007d56c79efec
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2012-04-14 20:46:26 -0500

    ConectarDosProcesos ya esta funcionando

--- src/ConectarDosProcesos.c
+++ src/ConectarDosProcesos.c
@@ -4,43 +4,67 @@
 int
 main(int argc, char *argv[]) {
 
-  STARTUPINFO startupInfoP1, startupInfoP2;
+  STARTUPINFO startupInfo;
   PROCESS_INFORMATION piProcInfoP1, piProcInfoP2;
   HANDLE hReadPipe, hWritePipe;
+  SECURITY_ATTRIBUTES saAttr;
 
-  GetStartupInfo(&startupInfoP1);
-  GetStartupInfo(&startupInfoP2);
+  saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+  saAttr.bInheritHandle = TRUE;
+  saAttr.lpSecurityDescriptor = NULL;
 
+  // GetStartupInfo(&startupInfoP1);
+  // GetStartupInfo(&startupInfoP2);
+
+  
   if (!CreatePipe(&hReadPipe   // hReadPipe
 		  ,&hWritePipe // hWritePipe
-		  ,NULL        // lpPipeAttributes
+		  ,&saAttr     // lpPipeAttributes
 		  ,0)) {       // nSize
     fprintf(stderr, "Error creando tuberias: %ld\n", GetLastError());
     ExitProcess((DWORD) 1); 
   }
 
-  startupInfoP1.hStdInput  = GetStdHandle(STD_INPUT_HANDLE);
-  startupInfoP1.hStdOutput = hWritePipe;
-  startupInfoP1.hStdError  = GetStdHandle(STD_ERROR_HANDLE);
-  startupInfoP2.hStdInput  = hReadPipe;
-  startupInfoP2.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-  startupInfoP2.hStdError  = GetStdHandle(STD_ERROR_HANDLE);
+  // Inicializando las estructuras de la informaci¢n de los procesos
+  ZeroMemory(&piProcInfoP1, sizeof(PROCESS_INFORMATION));
+
+  // Inicializando la informaci¢n que cada proceso necesita
+  ZeroMemory(&startupInfo, sizeof(STARTUPINFO));
+  startupInfo.cb = sizeof(STARTUPINFO);
+  startupInfo.hStdInput  = GetStdHandle(STD_INPUT_HANDLE);
+  startupInfo.hStdOutput = hWritePipe;
+  startupInfo.hStdError  = GetStdHandle(STD_ERROR_HANDLE);
+  startupInfo.dwFlags    |= STARTF_USESTDHANDLES;
 
   if (!CreateProcess(NULL, TEXT("MostrarArchivo"), NULL, NULL,
-                     FALSE, 0, NULL, NULL, &startupInfoP1, &piProcInfoP1)) {
+                     TRUE, 0, NULL, NULL, &startupInfo, &piProcInfoP1)) {
     fprintf(stderr, "Error creando proceso Mostrar Archivo: %ld\n", 
 	    GetLastError());
     ExitProcess((DWORD) 2);
   }
 
+  // Inicializando las estructuras de la informaci¢n de los procesos
+  ZeroMemory(&piProcInfoP2, sizeof(PROCESS_INFORMATION));
+
+  // Inicializando la informaci¢n que cada proceso necesita
+  ZeroMemory(&startupInfo, sizeof(STARTUPINFO));
+  startupInfo.cb = sizeof(STARTUPINFO);
+  startupInfo.hStdInput  = hReadPipe;
+  startupInfo.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+  startupInfo.hStdError  = GetStdHandle(STD_ERROR_HANDLE);
+  startupInfo.dwFlags    |= STARTF_USESTDHANDLES;
+
+
   if (!CreateProcess(NULL, TEXT("CatW"), NULL, NULL,
-                     FALSE, 0, NULL, NULL, &startupInfoP2, &piProcInfoP2)) {
+                     TRUE, 0, NULL, NULL, &startupInfo, &piProcInfoP2)) {
     fprintf(stderr, "Error creando proceso CatW: %ld\n", 
 	    GetLastError());
     ExitProcess((DWORD) 3);
   }
 
-  WaitForObject(piProcinfoP2.hProcess, 0);
+  
+  WaitForSingleObject(piProcInfoP1.hProcess, 0);
+  WaitForSingleObject(piProcInfoP2.hProcess, 0);
   ExitProcess((DWORD) 0);
 }
-  
+
