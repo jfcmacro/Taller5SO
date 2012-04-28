c906bec2aaeff013892598e9bf20586425ee7801
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2012-04-28 10:56:37 -0500

    Correcion a seccionCritica

--- src/seccionCritica.c
+++ src/seccionCritica.c
@@ -2,26 +2,29 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
 int nCrit = 0;
 
 void*
-proceso1(void* arg)
+hilo1(void* arg)
 {
   sem_t* semaforo = (sem_t *) arg;
-  
+
   for (;;) {
-    
+
     sem_wait(semaforo);
-    
+
     nCrit++;
     if (nCrit > 1) { printf("Error en seccion critica\n"); exit(0); }
     nCrit--;
-    
-    printf("Hilo: %d\n", pthread_self());
+
+    printf("Hilo 1: %ld\n", pthread_self());
     sem_post(semaforo);
+    sleep(random() % 5);
   }
-  
+
   return (void *) 0;
 }
 
@@ -29,7 +32,7 @@ void*
 proceso2(void *arg)
 {
   sem_t* semaforo = (sem_t *) arg;
-  
+
   for (;;) {
 
     sem_wait(semaforo);
@@ -37,9 +40,9 @@ proceso2(void *arg)
     nCrit++;
     if (nCrit > 1) { printf("Error en seccion critica\n"); exit(0); }
     nCrit--;
-    sleep(1);
-    printf("Hilo: %d\n", pthread_self());
+    printf("Hilo 2: %ld\n", pthread_self());
     sem_post(semaforo);
+    sleep(random() % 5);
   }
 
    return (void *) 0;
@@ -50,13 +53,14 @@ main(int argc, char *argv[])
 {
   sem_t semaforo;
   pthread_t thread1, thread2;
-  
+
+  srandom(time(NULL));
   sem_init(&semaforo, 0, 1);
-  
-  pthread_create(&thread1, NULL, proceso1, (void *) &semaforo);
-  
+
+  pthread_create(&thread1, NULL, hilo1, (void *) &semaforo);
+
   pthread_create(&thread2, NULL, proceso2, (void *) &semaforo);
-  
+
   pthread_join(thread1, NULL);
   pthread_join(thread2, NULL);
 }
